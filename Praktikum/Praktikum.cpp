384d7e7509ed3b8daeec51254e0857eacafa0df7
meshuggah69 <athariqzaky@gmail.com>
2024-03-07 15:41:45 +0700

    menambahkan input

--- Praktikum/Praktikum.cpp
+++ Praktikum/Praktikum.cpp
@@ -23,7 +23,7 @@ void input() {       //Procedure untuk input
 
     for (int i = 0; i < n; i++) {        //Looping dengan i dimulai dari 0 hingga n-1
         cout << "Data ke-" << (i + 1) << "; ";  //Output ke layar
-        cin >> a[i];         //Input dari pengguna
+        cin >> a[i];         //Input dari pengguna 
     }
 }
 
